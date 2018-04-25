fdb78c9911d7f189c4c3d9018bd757b94393fe32
Adwiti789 <adwiti1975@gmail.com>
2018-04-24 23:00:10 -0700

    Initial commit

---
+++ src/main/java/com/arun/ProjectStudentApplication.java
@@ -0,0 +1,12 @@
+package com.arun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStudentApplication.class, args);
+	}
+}
